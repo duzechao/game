9d07a1b9ada9026496ab1e626f168047b28ec9de
duzechao <duzechao@zuzuche.com>
2021-10-09 18:13:13 +0800

    1

--- src/components/Query.vue
+++ src/components/Query.vue
@@ -1,32 +1,85 @@
 
 <template>
   <div class="hello">
-    <a-input v-model="privateKey" placeholder="wallet key" />
-    <a-button type="primary" @click="query">query</a-button>
+    <div>已收入:{{weiRaised}}USDT</div>
     <div>address:{{address}}</div>
-    <div>balance:{{coinBalance}}</div>
+    <div>rate:{{rate}}</div>
+    <div>paymented:{{paymented}}</div>
 
   </div>
 </template>
 
 <script>
-import {balance, getAbi, loadWallet} from '../utils/walletUtil'
+import CrowdsaleByTokenAbi from "../config/CrowdsaleByTokenAbi.json";
+import * as ethers from "ethers";
+import Web3Modal from "web3modal";
+var provider;
+var signer;
+var contract;  
 export default {
   name: 'Query',
+  
   data () {
     return {
       address: '',
-      privateKey:'',
-      coinBalance:''
+      rate:'',
+      weiRaised:'',
+      paymented:"",
+      weiRaised:""
     }
   },
   methods:{
     query:async function(){
-      let wallet = loadWallet(this.$data.privateKey)
-      let b = await balance(wallet)
-      this.$data.address = wallet.address
-      this.$data.coinBalance = b
+      if(contract==undefined){
+          return;
+      }
+      let rate = await contract.getCurrentRate()
+      let address = await signer.getAddress()
+      let paymented = await contract.getPaymented(address)
+      let weiRaised = await contract._weiRaised()
+      this.$data.address = address
+      this.$data.rate = rate
+      this.$data.paymented = paymented/(10**18)
+      this.$data.weiRaised = weiRaised/(10**18)
+    },
+    onComplete(data){
+      console.log('data:', data);
+    },
+    initWallet:async function(){
+      
+      const providerOptions = {
+        /* See Provider Options Section */
+      };
+
+      const web3Modal = new Web3Modal({
+        network: "mainnet", // optional
+        cacheProvider: true, // optional
+        providerOptions // required
+      });
+
+      const web3provider = await web3Modal.connect();
+      provider = new ethers.providers.Web3Provider(web3provider)
+      signer = provider.getSigner()
+      contract = new ethers.Contract("0x4aD8Be742D2C820ED783da8E20a381F65F105cEA", CrowdsaleByTokenAbi, signer)
+      let that = this;
+      contract.on("TokensPurchased", (purchaser, beneficiary, value, amount,paymented) => {
+        console.log(`${ purchaser }  ${ beneficiary }  ${ value} ${amount}  ${paymented}`);
+        that.query();
+        });
+      web3provider.on('accountsChanged', function (accounts) {
+        that.query()
+      })
+      that.query()
+
     }
+  },
+  mounted(){
+    let that = this;
+    this.$nextTick(function () {
+      
+      that.initWallet()
+      
+  })
   }
 }
 </script>
